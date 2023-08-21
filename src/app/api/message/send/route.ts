e9f589b5ea4c06875076ec1adeeb43d83943df1b
DusceacBogdan <dusceacb@gmail.com>
2023-08-22 01:36:10 +0300

    .

--- src/app/api/message/send/route.ts
+++ src/app/api/message/send/route.ts
@@ -44,7 +44,7 @@ export async function POST(req: Request) {
         const message = messageValidator.parse(messageData);
 
         // notify all connected chat room clients
-        await pusherServer.trigger(toPusherKey(`chat:${chatId}`), 'incoming-message', message);
+        await pusherServer.trigger(toPusherKey(`chat:${chatId}`), 'incoming_message', message);
 
         await pusherServer.trigger(toPusherKey(`user:${friendId}:chats`), 'new_message', {
             ...message,
